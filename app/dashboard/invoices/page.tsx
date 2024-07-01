b7ff79b6b83be2d7799ff0a8893f5e3473b5540a
WESpekrul <wes.pekrul.oli@gmail.com>
2024-07-01 19:41:28 -0300

    194101072024

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,7 +9,7 @@ import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'Invoices',
+  title: 'Invoices Full',
 };
  
 export default async function Page({
